58a0f43f893cddc7b3776d16fb61b796aead8ac5
Gian_Paolo Pallari <gianpaolo.pallari@gmail.com>
2026-02-15 21:45:27 +0100

    Fix v0.6.0-beta.19: refine adaptive doctrine configuration to avoid enableNativeLazyObjects crash

--- tests/App/config/packages/doctrine.php
+++ tests/App/config/packages/doctrine.php
@@ -3,21 +3,38 @@
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container): void {
+    $ormConfig = [
+        'auto_mapping' => true,
+        'mappings' => [
+            'App' => [
+                'is_bundle' => false,
+                'type' => 'attribute',
+                'dir' => '%kernel.project_dir%/Entity',
+                'prefix' => 'Pallari\GeonameBundle\Tests\App\Entity',
+                'alias' => 'App',
+            ],
+        ],
+    ];
+
+    // Detect if we are on Doctrine ORM 3
+    $isOrm3 = !method_exists(\Doctrine\ORM\Configuration::class, 'getAutoGenerateProxyClasses');
+
+    if ($isOrm3) {
+        // In ORM 3, many legacy options are gone and handled by DoctrineBundle
+        $ormConfig['controller_resolver'] = ['auto_mapping' => false];
+        // We explicitly avoid any 'enable_lazy_ghost_objects' here
+    } else {
+        // ORM 2 specific safe settings
+        $ormConfig['auto_generate_proxy_classes'] = true;
+        // This is the key: we explicitly set it to false to avoid the internal call 
+        // to enableNativeLazyObjects in some versions of DoctrineBundle
+        $ormConfig['enable_lazy_ghost_objects'] = false;
+    }
+
     $container->extension('doctrine', [
         'dbal' => [
             'url' => '%env(resolve:DATABASE_URL)%',
         ],
-        'orm' => [
-            'auto_mapping' => true,
-            'mappings' => [
-                'App' => [
-                    'is_bundle' => false,
-                    'type' => 'attribute',
-                    'dir' => '%kernel.project_dir%/Entity',
-                    'prefix' => 'Pallari\GeonameBundle\Tests\App\Entity',
-                    'alias' => 'App',
-                ],
-            ],
-        ],
+        'orm' => $ormConfig,
     ]);
 };
