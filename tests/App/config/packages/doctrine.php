e5f177bf5f82c86c14c5121bd1405f63d4c12594
Gian_Paolo Pallari <gianpaolo.pallari@gmail.com>
2026-02-15 22:40:09 +0100

    Fix v0.6.0-beta.22: properly isolate CI matrix by PHP version and DoctrineBundle requirements

--- tests/App/config/packages/doctrine.php
+++ tests/App/config/packages/doctrine.php
@@ -16,30 +16,15 @@ return static function (ContainerConfigurator $container): void {
         ],
     ];
 
-    // Detect Doctrine ORM version
-    $isOrm3 = !method_exists(\Doctrine\ORM\Configuration::class, 'getAutoGenerateProxyClasses');
-    
-    // Detect Doctrine Bundle capabilities by looking at its configuration class
-    // This is more reliable than checking the version string
-    $bundleConfigClass = new \ReflectionClass(\Doctrine\Bundle\DoctrineBundle\DependencyInjection\Configuration::class);
-    $configMethod = $bundleConfigClass->getMethod('getConfigTreeBuilder');
-    // We won't parse the whole tree, but we can check for ORM 3.0 specific changes
+    // Detect if we are on DoctrineBundle 3.0+
     $isBundle3 = !class_exists(\Doctrine\Bundle\DoctrineBundle\Command\Proxy\ImportDoctrineProxyCommand::class);
 
-    if ($isOrm3) {
-        // ORM 3: most legacy proxy/lazy settings are now handled automatically or moved
+    if ($isBundle3) {
+        // Modern configuration for DoctrineBundle 3.0 (PHP 8.4+)
         $ormConfig['controller_resolver'] = ['auto_mapping' => false];
     } else {
-        // ORM 2
-        if (!$isBundle3) {
-            // Option only available in Bundle < 3.0
-            $ormConfig['auto_generate_proxy_classes'] = true;
-        }
-        
-        // ghost objects option was only relevant for Bundle 2.12+ with ORM 2
-        if (class_exists(\Doctrine\Bundle\DoctrineBundle\Controller\ArgumentResolver\EntityValueResolver::class)) {
-             $ormConfig['enable_lazy_ghost_objects'] = true;
-        }
+        // Legacy configuration for DoctrineBundle 2.x
+        $ormConfig['auto_generate_proxy_classes'] = true;
     }
 
     $container->extension('doctrine', [
